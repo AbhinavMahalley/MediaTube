1ade3ec04eeb9143d1d4c6b9eae3e37dabf92143
Abhinav Mahalley <85456745+AbhinavMahalley@users.noreply.github.com>
2025-02-16 20:06:49 +0530

    Update NavBar.jsx logo url

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 // import { logo } from '../utils/constants';
-import { logo } from 'public/media-tube-logo.png';
-// import { logo } from 'https://i.ibb.co/3mXQHCnS/media-tube-logo.png';
+import { logo } from 'https://i.ibb.co/3mXQHCnS/media-tube-logo.png';
 import SearchBar from './SearchBar';
 
 const NavBar = () => (
         <Stack direction="row" alignItems="center"  p={1} px={2} sx={{position: 'sticky', background: '#000', top: 0, 
         justifyContent: 'space-between',}}>
             <Link to="/" style={{display: 'flex', alignItems: 'center',color:'white'}}>
-{/*                 <img src={logo} alt="logo" height={45} /> */}
                 <img src={logo} alt="logo" height={45} />
                <h1 className="title-heading" style={{fontWeight:'bold', marginLeft:'2.5px'}}> MediaTube </h1>
             </Link>
